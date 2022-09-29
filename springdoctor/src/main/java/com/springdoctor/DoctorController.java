ba69dc7e29bfd9436833669044fe4b7a92af7dd9
Armenio Andrade <armenioandrade@gmail.com>
2022-09-29 19:19:38 -0300

    delete

--- springdoctor/src/main/java/com/springdoctor/DoctorController.java
+++ springdoctor/src/main/java/com/springdoctor/DoctorController.java
@@ -5,12 +5,10 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 import org.springframework.web.servlet.view.RedirectView;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
+import javax.persistence.*;
 import javax.print.Doc;
 
 
@@ -22,7 +20,14 @@ public class DoctorController {
 
     @GetMapping(path = "")
     public String index(Model model){
-        System.out.println("chegou no index");
+        System.out.println("chegou no index sem barra");
+        model.addAttribute("lista", doctorRepository.findAll());
+        return "index";
+    }
+
+    @GetMapping(path = "/")
+    public String indexBarra(Model model){
+        System.out.println("chegou no index sem barra");
         model.addAttribute("lista", doctorRepository.findAll());
         return "index";
     }
@@ -35,20 +40,23 @@ public class DoctorController {
     }
 
     @PostMapping(path = "/addDoctor")
-    public String addDoctorSubmit(@ModelAttribute Doctor doctor, Model model){
+    public String addDoctorSubmit(@ModelAttribute Doctor doctor, Model model, RedirectAttributes redirectAttributes){
         System.out.println("chegou no postmapping adddoctor");
         doctorRepository.save(doctor);
-        return "cadastro";
+        redirectAttributes.addFlashAttribute("mensagem", "usuario cadastrado!");
+        return "redirect:/Doctor/";
     }
 
     @GetMapping(path = "/DeleteDoctor")
-    public String deleteDoctor(@ModelAttribute Doctor doctor, Model model){
+    public String deleteDoctor(@RequestParam int doctorId){
         System.out.println("chegou no metodo deletar");
-        doctorRepository.delete(doctor);
-        return "/";
+        System.out.println("id "+doctorId);
+        doctorRepository.deleteById(doctorId);
+        return "redirect:/Doctor/";
     }
 
     @Entity
+    @Table(name = "Doctor")
     public static class Doctor {
         @Id
         @GeneratedValue(strategy= GenerationType.AUTO)
